dfca41b1754cc77aac3dbcad0820fa0ae4333af4
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2022-04-28 15:45:36 +0900

    Create main.cpp

---
+++ 1100 하얀 칸/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+char board[8][8];
+int result = 0;
+
+int main(void){
+	for(int i = 0; i < 8; i++){
+		for(int j = 0; j < 8; j++){
+			scanf(" %c", &board[i][j]);
+		}
+		"\n";
+	}
+	for(int i = 0; i < 8; i++){
+		for(int j = 0; j < 8; j++){
+			if((i+j)%2==0 && board[i][j] == 'F'){ 
+				result++; 
+			}
+		}
+	}
+	printf("%d\n", result);
+	return 0;
+}
