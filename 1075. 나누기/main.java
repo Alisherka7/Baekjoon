9c567e51a252806bab9c7731c25fcf5719f95c33
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2023-01-03 01:20:52 +0900

    Create main.java

---
+++ 1075. 나누기/main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int f = Integer.parseInt(br.readLine());
+        int t = (n/100)*100;
+        while(true) {
+            if(t%f == 0) {
+                int result = t % 100;
+                if(result < 10){
+                    System.out.println("0" + result);
+                }
+                else{
+                    System.out.println(result);
+                }
+                return;
+            }
+            t++;
+        }
+        
+    }
+}
