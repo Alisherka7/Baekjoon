ff5f932fc5afa1bb6ccb113f8a7b5810d9047d9c
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2023-01-17 22:13:22 +0900

    Update main.java

--- 1392. 노래 악보/main.java
+++ 1392. 노래 악보/main.java
@@ -6,8 +6,8 @@ public class Main{
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int q = sc.nextInt();
+	int n = sc.nextInt();
+	int q = sc.nextInt();
 
         for(int i =0; i<n; i++){
             arrN[i] = sc.nextInt();
